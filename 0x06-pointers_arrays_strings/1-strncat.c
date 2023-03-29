48738499a938e4c8668330f6f96598a67e7c790b
lovinaj <lovejon218@gmail.com>
2023-03-29 13:21:21 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,16 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int destlen = 0;
-int srclen = 0;
 int i;
 
-for (i = 0; dest[i] != '\0'; i++)
-destlen++;
-for (i = 0; src[i] != '\0'; i++)
-srclen++;
-for (i = 0; i < n; i++)
-dest[destlen + i] = src[i];
+while (dest[destlen] != '\0')
+{
+	destlen++;
+}
+for (i = 0; i < n &&  src[i] != '\0'; i++, destlen++)
+{
+	dest[destlen] = src[i];
+}
+dest[destlen] = '\0';
 return (dest);
 }
