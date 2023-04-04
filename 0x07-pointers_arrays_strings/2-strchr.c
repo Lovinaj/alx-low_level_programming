caeedb51aa2b1d7b13f08a9e66e0a5c242d48387
lovinaj <lovejon218@gmail.com>
2023-04-03 17:31:28 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 
 /**
 * _strchr - function that locates a character in a string
-* @s: string to be checked
+* @s: string to be searched
 * @c: character to be located
 * Returns: a pointer to the first occurrence of the character c
 * in the string s, or NULL if the character is not found
